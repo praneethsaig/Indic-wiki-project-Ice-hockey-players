19dcd8a9c18fce00d4165d66ce9fb40d5aba2973
praneethsaig <70117861+praneethsaig@users.noreply.github.com>
2022-03-25 20:12:28 +0530

    Update makepkl.py

--- makepkl.py
+++ makepkl.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # Makes a .pkl file which can be for loading data into the template
 def main():
-    plantsFile = 'data/Telugu.csv'
-    plantsDF = pd.read_csv(plantsFile)
-    pickle.dump(plantsDF, open('./123.pkl', 'wb'))
+    HockeyFile = 'data/players_datafinal.csv'
+    hockeyDF = pd.read_csv(hockeyFile)
+    pickle.dump(hockeyDF, open('./123.pkl', 'wb'))
 
 if __name__ == '__main__':
     main()
