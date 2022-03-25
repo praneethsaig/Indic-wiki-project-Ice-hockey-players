acc8e754c17eb7f55765d229efa0dafab5a92fd9
praneethsaig <70117861+praneethsaig@users.noreply.github.com>
2022-03-25 20:08:49 +0530

    Update render.py

--- render.py
+++ render.py
@@ -1,3 +1,5 @@
+!pip install tewiki, writePage
+
 import pickle
 import ast
 from jinja2 import Environment, FileSystemLoader
@@ -7,48 +9,46 @@ from genXML import tewiki, writePage
 
 def getData(row,i):
 	data = {
-		'Index': row.Index.values[0],
-		'Synonyms': (row.Synonyms.values[0]),
-		'Latin_name':row.Latin_name.values[0],
-		'Family':row.Family.values[0],
-		'Common_name':row.Common_name.values[0],
-		'Habit':row.Habit.values[0],
-		'Deciduous/Evergreen':row.Deciduous_Evergreen.values[0],
+		'Index': (row.Index.values[0]),
+		'Name':row.Name.values[0],
+		'Date_of_birth':row.Date_of_birth.values[0],
+		'Season_year':row.Season_year.values[0],
+		'Season':row.Season.values[0],
+		'Number':row.Number.values[0],
+		'Team':(row.Team.values[0]),	
+		'Games_Played':row.Games_Played.values[0],
+		'Goals':row.Goals.values[0],
+		'Assists':row.Assists.values[0],
+		'Points':row.Points.values[0],
+		'PlusMinus_Ratings':row.PlusMinus_Ratings.values[0],
+		'Penalty_Minutes':row.Penalty_Minutes.values[0],
+		'Shots_on_Goal':row.Shots_on_Goal.values[0],
+		'Shooting_Percentage':row.Shooting_Percentage.values[0],
+		#'PowerPlay_Goals':(row.PowerPlay_Goals.values[0]),
+		'PowerPlay_Assists':row.PowerPlay_Assists.values[0],
+		'Short_Goals':row.Short_Goals.values[0],
+		'Short_Assists':row.Short_Assists.values[0],
+		'Game_Winning_Goals':row.Game_Winning_Goals.values[0],
+		'Game_Tying_Goals':row.Game_Tying_Goals.values[0],
+		'Time_on_Ice_per_Game':row.Time_on_Ice_per_Game.values[0],
+		'Games_Started': row.Games_Started.values[0],
+		'Wins':row.Wins.values[0],
+		'Losses':row.Losses.values[0],
+		'Ties':row.Ties.values[0],
+		'Overtime_Losses':row.Overtime_Losses.values[0],
+		'Goals_Against':row.Goals_Against.values[0],
+		'Goals_Against_Average': row.Goals_Against_Average.values[0],
+		'Shots_Against': row.Shots_Against.values[0],
+		'Saves': row.Saves.values[0],
+		'Save_Percentage':row.Save_Percentage.values[0],
+		'Shutouts':row.Shutouts.values[0],
+		'Position':row.Position.values[0],
 		'Height':row.Height.values[0],
-		'Width':row.Width.values[0],
-		'UK_Hardiness':row.UK_Hardiness.values[0],
-		'Medicinal':row.Medicinal.values[0],
-		'Range':row.Range.values[0],
-		'Habitat':row.Habitat.values[0],
-		'Soil':row.Soil.values[0],
-		'Shade':row.Shade.values[0],
-		'Moisture':row.Moisture.values[0],
-		'Well_drained':row.Well_drained.values[0],
-		'Nitrogen_fixer':row.Nitrogen_fixer.values[0],
-		'pH':row.pH.values[0],
-		'Acid':row.Acid.values[0],
-		'Alkaline':row.Alkaline.values[0],
-		'Saline': row.Saline.values[0],
-		'Wind':row.Wind.values[0],
-		'Growth_rate':row.Growth_rate.values[0],
-		'Pollution':row.Pollution.values[0],
-		'Poor_soil':row.Poor_soil.values[0],
-		'Drought':row.Drought.values[0],
-		'Wildlife': row.Wildlife.values[0],
-		'Pollinators': row.Pollinators.values[0],
-		'Self_fertile': row.Self_fertile.values[0],
-		'Known_hazards':row.Known_hazards.values[0],
-		'Cultivation_details':row.Cultivation_details.values[0],
-		'Edible_uses':row.Edible_uses.values[0],
-		'Uses_notes':row.Uses_notes.values[0],
-		'Heavy_clay':row.Heavy_clay.values[0],
-		'Propagation':row.Propagation.values[0],
-		'Edibility_rating':row.Edibility_rating.values[0],
-		'Frost_Tender':row.Frost_Tender.values[0],
-		'Scented':row.Scented.values[0],
-		'Medicinal_rating':row.Medicinal_rating.values[0],
-		'Images':row.Images.values[0],
-		'Imagesthumb':row.Imagesthumb.values[0]
+		'Weight':row.Weight.values[0],
+		'Body_mass_index':row.Body_mass_index.values[0],
+		'Place_of_birth':row.Place_of_birth.values[0],
+		'Age':row.Age.values[0],
+		'Experience':row.Experience.values[0]
 	}
 	return data
 
@@ -61,14 +61,15 @@ def main():
 	template = env.get_template('main_template.j2')
 
 	# Load the data (.pkl)
-	plantsDF =pickle.load(open('./123.pkl', 'rb'))
+	hockeyDF =pickle.load(open('./123.pkl', 'rb'))
 
 
-	#remove this to generate articles for all plants
-	ids = plantsDF.Index.tolist()
+	#remove this to generate articles for all movies
+	ids = hockeyDF.Index.tolist()
+	#ids = ids[:1]
 	# print(ids)
 
-	# print(type(plantsDF.iloc[0]))
+	# print(type(hockeyDF.iloc[0]))
 	# Initiate the file object
 	fobj = open('1.xml', 'w')
 	fobj.write(tewiki+'\n')
@@ -78,8 +79,8 @@ def main():
 
 	# Loop to grab all data from the .pkl and generate articles using the template
 	for i in range(len(ids)):
-		row = plantsDF.loc[(plantsDF['Index']==i+1)]
-		title = row.Latin_name.values[0]
+		row = hockeyDF.loc[(hockeyDF['Index']==i+1)]
+		title = row.Name.values[0]
 		text = template.render(getData(row,i))
 		# print(text)
 
